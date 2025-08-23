86191d0340f94fd64c19a801e0b45cbd3906078d
Melbin Mathew <melbinppmathewp@gmail.com>
2025-08-23 21:25:01 +0530

    20th commit

--- src/App.tsx
+++ src/App.tsx
@@ -20,36 +20,51 @@ import FloatingActions from "./components/contact-buttons";
 import Rentals from "./pages/services/rentals";
 import ContactUs from "./pages/contact-us";
 import NotFound404 from "./pages/404";
+import { useEffect, useState } from "react";
+import LogoLoader from "./components/loader";
 
 function App() {
   const location = useLocation();
   const path = location.pathname;
 
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [location]);
+
   return (
     <>
+      {loading && <LogoLoader />}
       <Navbar />
       {path !== "/" && <Breadcrumbs />}
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/about-us" element={ <About /> } />
-        <Route path="/contact-us" element={ <ContactUs /> } />
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/contact-us" element={<ContactUs />} />
 
-        <Route path="/products" element={ <Products /> } />
-        <Route path="/products/water-chillers" element={ <WaterChillers /> } />
-        <Route path="/products/cold-rooms" element={ <ColdRooms /> } />
-        <Route path="/products/heat-exchangers" element={ <HeatExchangers /> } />
-        <Route path="/products/ac-units" element={ <ACUnits /> } />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/water-chillers" element={<WaterChillers />} />
+        <Route path="/products/cold-rooms" element={<ColdRooms />} />
+        <Route path="/products/heat-exchangers" element={<HeatExchangers />} />
+        <Route path="/products/ac-units" element={<ACUnits />} />
 
-        <Route path="/services" element={ <Services /> } />
-        <Route path="/services/rentals" element={ <Rentals /> }/>
-        <Route path="/services/pipeline" element={ <ChilledWaterPipelineAndFlushing /> } />
-        <Route path="/services/maintenance" element={ <ACAndChillerMaintenance /> } />
-        <Route path="/services/coil" element={ <CoilManufacturingAndReplacement /> } />
-        <Route path="/services/overhauling" element={ <OverhaulingAndWinding /> } />
-        <Route path="/services/amc" element={ <AMC /> } />
+        <Route path="/services" element={<Services />} />
+        <Route path="/services/rentals" element={<Rentals />} />
+        <Route path="/services/pipeline" element={<ChilledWaterPipelineAndFlushing />} />
+        <Route path="/services/maintenance" element={<ACAndChillerMaintenance />} />
+        <Route path="/services/coil" element={<CoilManufacturingAndReplacement />} />
+        <Route path="/services/overhauling" element={<OverhaulingAndWinding />} />
+        <Route path="/services/amc" element={<AMC />} />
 
-        <Route path="*" element={ <NotFound404 /> } />
+        <Route path="*" element={<NotFound404 />} />
       </Routes>
       <FloatingActions />
       <Footer />
