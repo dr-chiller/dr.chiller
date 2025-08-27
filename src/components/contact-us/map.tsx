1502a3ce66553bfbb4230fd350032926d7c8a2af
Melbin Mathew <melbinppmathewp@gmail.com>
2025-08-27 13:59:21 +0530

    changed the map location in contact page

--- src/components/contact-us/map.tsx
+++ src/components/contact-us/map.tsx
@@ -2,7 +2,7 @@ const ContactUsMap = () => {
     return (
         <div className="w-full h-96 overflow-hidden shadow">
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3604.681182369924!2d55.47164927538777!3d25.381999477592508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDIyJzU1LjIiTiA1NcKwMjgnMjcuMiJF!5e0!3m2!1sen!2sin!4v1755591237097!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d185.69043049580569!2d55.474328!3d25.381956!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f58667d25286b%3A0x950fd127e21e0a5f!2sChiller%20Technical%20Services%20LLC!5e1!3m2!1sen!2sus!4v1756282696652!5m2!1sen!2sus"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
